ed670efa071fb78b12c2b2387eea9f6c98cd7cad
protectorT1 <tarapokhara@gmail.com>
2017-04-07 13:35:32 +0100

    last submissions done

--- last code.R
+++ last code.R
@@ -35,18 +35,22 @@ log_loss(validate$response, validate_pred)
 
 
 baseline_model <- gbm(
-  formula=response ~ burglary +rodents + garbage + foodType + sanitation + facilityType + pastFail + zipCode + facilityType + catersLiquorLicense + ward,
+  formula=response ~ sanitation +rodents + zipCode,
   distribution='bernoulli',
   data=train,
-  n.trees=10000,
+  n.trees=10,
   interaction.depth=7,
   n.minobsinnode=5,
-  shrinkage=0.001,
+  shrinkage=0.01,
   bag.fraction=0.75,
-  train.fraction=0.7,
+  train.fraction=0.5,
   keep.data=FALSE,
   verbose=TRUE
 )
+
+summary(baseline_model, plot = FALSE)
+summary(baseline_model, plot = TRUE)
+
 num_trees <- gbm.perf(baseline_model, method='test')
 train_pred <- predict(baseline_model, train, n.trees=num_trees, type='response')
 validate_pred <- predict(baseline_model, validate, n.trees=num_trees, type='response')
@@ -70,3 +74,78 @@ summary(vectorOfPredictions)
   head(outputDataSet)
   
   write.csv(outputDataSet, "submission3.csv", row.names = FALSE)
+  
+  dim(train_data)
+  dim(test_data)
+  
+  v <- (train_data$sanitation + train_data$rodents + train_data$zipCode)
+  
+  vector = c()
+  for (item in v)
+  {
+    item <- gbm(
+      formula=response ~ item,
+      distribution='bernoulli',
+      data=train,
+      n.trees=10000,
+      interaction.depth=7,
+      n.minobsinnode=10,
+      shrinkage=0.01,
+      bag.fraction=0.75,
+      train.fraction=0.5,
+      keep.data=FALSE,
+      verbose=TRUE
+    )
+    vector <- c(vector, log_loss(validate$response, validate_pred))
+    return(vector)
+  }
+  
+  sanitation_model <- gbm(
+    formula=response ~ sanitation,
+    distribution='bernoulli',
+    data=train,
+    n.trees=50,
+    interaction.depth=7,
+    n.minobsinnode=5,
+    shrinkage=0.01,
+    bag.fraction=0.75,
+    train.fraction=0.5,
+    keep.data=FALSE,
+    verbose=TRUE
+  )
+  vector_sanitation <- c( log_loss(validate$response, validate_pred))
+  
+  rodents_model <- gbm(
+    formula=response ~ rodents,
+    distribution='bernoulli',
+    data=train,
+    n.trees=50,
+    interaction.depth=7,
+    n.minobsinnode=5,
+    shrinkage=0.01,
+    bag.fraction=0.75,
+    train.fraction=0.5,
+    keep.data=FALSE,
+    verbose=TRUE
+  )
+  vector_rodents <- c( log_loss(validate$response, validate_pred))
+  
+  zipCode_model <- gbm(
+    formula=response ~ zipCode,
+    distribution='bernoulli',
+    data=train,
+    n.trees=50,
+    interaction.depth=7,
+    n.minobsinnode=5,
+    shrinkage=0.01,
+    bag.fraction=0.75,
+    train.fraction=0.5,
+    keep.data=FALSE,
+    verbose=TRUE
+  )
+  vector_zipCode <- c( log_loss(validate$response, validate_pred))
+  
+  total <- (vector_sanitation + vector_rodents + vector_zipCode)/3
+  print(total)
+  
+  write.csv(outputDataSet, "submission4.csv", row.names = FALSE)
